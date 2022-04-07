d3838b64626f31265986f8c81e152de8fbd41439
famirseidov <amirseidov@profiterole.group>
2022-04-07 18:12:03 +0300

    feat: added type declarations for assets, tsconfig changes

---
+++ src/tokens.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ICONS } from "./globalStyle/icons";
+
+export const TOKENS = {
+  ethereum: {
+    name: "Ethereum",
+    icon: <ICONS.Ethereum />,
+  },
+  bitcoin: {
+    name: "Bitcoin",
+    icon: <ICONS.Bitcoin />,
+  },
+};
