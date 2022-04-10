06efd277e09a7a2974db168b1edd0de14c7a3bd1
famirseidov <amirseidov@profiterole.group>
2022-04-11 02:31:02 +0300

    feat: layout changes

--- src/components/template/Layout/index.tsx
+++ src/components/template/Layout/index.tsx
@@ -5,13 +5,26 @@ import * as S from "./style";
 interface LayoutProps {
   children: ReactNode;
   footer?: ReactNode;
+  footerBackground?: boolean;
+  style?: React.CSSProperties;
+  className?: string;
 }
 
-export const Layout = ({ children, footer }: LayoutProps) => {
+export const Layout = ({
+  children,
+  footer,
+  footerBackground = true,
+  style,
+  className,
+}: LayoutProps) => {
   return (
     <>
-      <S.Layout>{children}</S.Layout>
-      {footer && <S.Footer>{footer}</S.Footer>}
+      <S.Layout className={className} style={style}>
+        {children}
+      </S.Layout>
+      {footer && (
+        <S.Footer footerBackground={footerBackground}>{footer}</S.Footer>
+      )}
     </>
   );
 };
--- src/components/template/Layout/style.ts
+++ src/components/template/Layout/style.ts
@@ -5,9 +5,16 @@ import { COLORS } from "@globalStyle/colors";
 export const Layout = styled.section`
   background-color: ${COLORS.background};
   padding: 1.5rem;
-  min-width: 22.5rem;
+  width: 22.5rem;
+  min-height: 600px;
+  position: relative;
 `;
-export const Footer = styled.footer`
-  background: rgba(255, 255, 255, 15%);
-  padding: 1.5rem;
+export const Footer = styled.footer<{ footerBackground: boolean }>`
+  background: ${({ footerBackground }) =>
+    footerBackground ? "rgba(255, 255, 255, 15%)" : "transparent"};
+  padding: 1rem 1.5rem 1.25rem;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
 `;
