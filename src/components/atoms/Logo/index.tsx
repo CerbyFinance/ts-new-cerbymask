470ecea3ebc3c6dea3926926ac7098a52e7a46fe
famirseidov <amirseidov@profiterole.group>
2022-05-10 03:25:40 +0300

    feat: new logo

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import { Title } from "./style";
 
-import Wolf from "@assets/img/logo.png";
+import Wolf from "@assets/svg/wolf.svg";
 
 export const Logo = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <img src={Wolf} alt="CerbyMask logo" width="50%" height="50%" />
+      <Wolf />
       <Title>Cerby</Title>
       <p
         style={{ marginTop: ".5rem", fontFamily: "Jost", fontSize: ".825rem" }}
